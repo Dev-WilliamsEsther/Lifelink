a3780b6f0dc72cb5160762b4fdb9d77a577b3580
CodeAdio <145165239+opeyemi2007@users.noreply.github.com>
2025-04-07 01:49:55 +0100

    Update Howitworks.jsx

--- src/Adio/Howitworks.jsx
+++ src/Adio/Howitworks.jsx
@@ -5,8 +5,8 @@ const Howitworks = () => {
   return (
     <>
       <div>
-        <section class="lifelink-Howitworks-first-section">
-          <div class="lifelink-text">
+        <section className="lifelink-Howitworks-first-section">
+          <div className="lifelink-text">
             <h2>Saving Lives, Made <br /> Simple</h2>
             <p>
               Whether you’re a donor looking to make a <br />
@@ -19,91 +19,91 @@ const Howitworks = () => {
               works:
             </p>
           </div>
-          <div class="lifelink-images">
+          <div className="lifelink-images">
             <img
               src="/public/howitworksHeroImage.png"
               alt="Patient in hospital bed"
-              class="img-back"
+              className="img-back"
             />
-            <img src="/public/howitworksHeroImage2.png" alt="Blood donation" class="img-front" />
+            <img src="/public/howitworksHeroImage2.png" alt="Blood donation" className="img-front" />
           </div>
         </section>
 
 
-        <section class="hospital-steps">
-          <h2 class="section-title">HOW IT WORKS FOR DONORS</h2>
+        <section className="hospital-steps">
+          <h2 className="section-title">HOW IT WORKS FOR DONORS</h2>
 
-          <div class="steps-container">
-            <div class="step-box step-1">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+          <div className="steps-container">
+            <div className="step-box step-1">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Find a Donation <br /> Center</h3>
               <p>Locate a nearby verified hospital or blood bank using LifeLink</p>
             </div>
 
-            <div class="step-box step-2">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+            <div className="step-box step-2">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Donate Blood</h3>
               <p>Visit the center provide your unique code, & give blood safely.</p>
             </div>
 
-            <div class="step-box step-3">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+            <div className="step-box step-3">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Track Your Impact</h3>
               <p>Get updates on your donation history & see how many lives you’ve helped saved.</p>
             </div>
 
-            <div class="step-box step-4">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+            <div className="step-box step-4">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Enjoy Donor Benefits</h3>
               <p>Receive incentives like free health checkups & wellness perks as a token of appriciation.</p>
             </div>
           </div>
 
-          <button class="cta-btn">Donate</button>
+          <button className="cta-btn">Donate</button>
         </section>
 
 
-        <section class="hospital-steps">
-          <h2 class="section-title">How It Works For Hospital & Blood Banks</h2>
+        <section className="hospital-steps">
+          <h2 className="section-title">How It Works For Hospital & Blood Banks</h2>
 
-          <div class="steps-container">
-            <div class="step-box step-1">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+          <div className="steps-container">
+            <div className="step-box step-1">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Register &<br />Get Verified</h3>
               <p>Sign up on LifeLink & get verified as a trusted hospital or blood bank.</p>
             </div>
 
-            <div class="step-box step-2">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+            <div className="step-box step-2">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Update Blood<br />Availability</h3>
               <p>Keep donors and patients informed by updating your blood supply in real time.</p>
             </div>
 
-            <div class="step-box step-3">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+            <div className="step-box step-3">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Receive Donors</h3>
               <p>Accept donors who visit with their unique LifeLink codes & confirm their donations.</p>
             </div>
 
-            <div class="step-box step-4">
-              <div class="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
+            <div className="step-box step-4">
+              <div className="icon"><img src="/public/cryptocurrency-color_gold.png"/></div>
               <h3>Save More Lives</h3>
               <p>Easily connect with donors & ensure a steady supply of safe & reliable blood.</p>
             </div>
           </div>
 
-          <button class="cta-btn">Get Listed</button>
+          <button className="cta-btn">Get Listed</button>
         </section>
 
-        <section class="why-it-matters">
-          <h2 class="section-title">WHY IT MATTERS</h2>
+        <section className="why-it-matters">
+          <h2 className="section-title">WHY IT MATTERS</h2>
 
-          <div class="cards">
-            <div class="card card-1"></div>
+          <div className="cards">
+            <div className="card card-1"></div>
 
-            <div class="card card-2"></div>
+            <div className="card card-2"></div>
 
-            <div class="card card-3"></div>
+            <div className="card card-3"></div>
           </div>
         </section>
 
