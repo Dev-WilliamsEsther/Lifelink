c6e6e9274bf89f2b0a5c238a9f95068c0e93f26b
Dev-WilliamsEsther <williexther20@gmail.com>
2025-04-07 19:19:05 +0100

    KEY BENEFITS FOR DONORS

---
+++ src/components/Benefit/Benefit.jsx
@@ -0,0 +1,70 @@
+import "../Benefit/Benefit.css";
+import Sponsors from "../Sponsors/Sponsors";
+
+const Benefit = () => {
+  return (
+    <>
+    <div className="landingpagebenefits">
+      <h1>KEY BENEFITS FOR DONORS</h1>
+      <div className="landingbenefitsinner">
+        <div className="landingbenefitsinner1">
+          <h2>Saves lives while gaining valuable benefits.</h2>
+          <p>
+            When you donate blood, you’re not just <br /> saving lives you’re
+            also gaining valuable <br /> benefits. Make an impact while taking
+            care <br /> of your own well-being.
+          </p>
+          <button className="benefitsbtn">Donate</button>
+        </div>
+        <div className="landingbenefitsinner2">
+          <div className="landingincentives">
+            <div className="incentiveheader">
+              <img src="public/images/Group.png" alt="" />
+            </div>
+            <h2>
+              Exclusive Donor <br /> Incentives
+            </h2>
+            <p>
+              Free basic health checkups (e.g blood pressure, general wellnes)
+            </p>
+          </div>
+          <div className="landingincentives">
+            <div className="incentiveheader">
+              <img src="public/images/Group.png" alt="" />
+            </div>
+            <h2>
+              Exclusive Donor <br /> Incentives
+            </h2>
+            <p>
+              Reduces the risk of heartdiseses <br />
+              and some cancers
+            </p>
+          </div>
+          <div className="landingincentives">
+            <div className="incentiveheader">
+              <img src="public/images/Group.png" alt="" />
+            </div>
+            <h2>
+              Exclusive Donor <br /> Incentives
+            </h2>
+            <p>Enhanced mental well-being</p>
+          </div>
+          <div className="landingincentives">
+            <div className="incentiveheader">
+              <img src="public/images/Group.png" alt="" />
+            </div>
+            <h2>
+              Exclusive Donor <br /> Incentives
+            </h2>
+            <p>
+              Provision of valuable information <br /> about your health.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Benefit;
---
+++ src/components/Sponsors/Sponsors.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../Sponsors/sponsor.css'
+
+const Sponsors = () => {
+  return (
+    <>
+    <div className='sponsorwrapper'>
+        <h1>SPONSORS</h1>
+        <div className="sponsorsLogos">
+            <marquee>
+                <p>Images</p>
+                <p>Images</p>
+                <p>Images</p>
+                <p>Images</p>
+            </marquee>
+        </div>
+    </div>
+    <div className='sponsorwrapper1'>Sponsors</div>
+    </>
+  )
+}
+
+export default Sponsors
