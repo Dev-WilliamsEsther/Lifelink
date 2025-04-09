0bfed8ea310ecf5843762dfac89c5a4709e5eb7f
Dev-WilliamsEsther <williexther20@gmail.com>
2025-04-09 11:50:54 +0100

    davidsongetsense

---
+++ src/components/impacts/Impacts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../components/impacts/impacts.css'
+
+const Impacts = () => {
+  return (
+    <div className='impactswrapper'></div>
+  )
+}
+
+export default Impacts
