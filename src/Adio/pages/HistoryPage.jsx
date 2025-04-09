77b93d846d20cc5d072d55a672817ca04ba5f410
CodeAdio <145165239+opeyemi2007@users.noreply.github.com>
2025-04-09 17:25:51 +0000

    almost done with dashboard

--- src/Adio/pages/HistoryPage.jsx
+++ src/Adio/pages/HistoryPage.jsx
@@ -1,11 +1,172 @@
 import React from 'react'
 import './historyPage.css'
+import { SlCalender } from "react-icons/sl";
 
 const HistoryPage = () => {
   return (
     <div className='HistoryPageWrapper'>
-      HistoryPage
-      HistoryPage
+      <h1>Pending Donation</h1>
+
+      <div className="DonationsHistoryCardsHeading">
+        <div className="DonationsHistoryCardsInnerWrapper">
+          <h1>Facilitity Name</h1>
+          <h1>VIEW DETAILS</h1>
+          <h1><SlCalender />DATE</h1>
+          <h1>LOCATION</h1>
+          <h1>STATUS</h1>
+        </div>
+      </div>
+
+
+      <div className="DonationsHistoryCardsWrapper">
+
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+        <div className="DonationsHistoryCards">
+          <div className="DonationsHistoryCardsInnerWrapper">
+            <h3>Kings Hospital</h3>
+            <h3>VIEW DETAILS</h3>
+            <h3><SlCalender />MAy 23, 2025</h3>
+            <h3>Ojo road, Lagos</h3>
+            <h3>Pending</h3>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   )
 }
