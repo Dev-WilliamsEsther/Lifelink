ebb36048deb3af6a9a197a0b0f645b06e5ff22ba
urigwesomto201 <coryguerrero500@gmail.com>
2025-04-11 05:46:45 -0500

    mobile screen almost done

--- src/Adio/pages/ProfilePage.jsx
+++ src/Adio/pages/ProfilePage.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import './profilePage.css'
 import { MdCloudUpload } from "react-icons/md";
--- src/Adio/pages/SettingsPage.jsx
+++ src/Adio/pages/SettingsPage.jsx
@@ -14,10 +14,34 @@ const SettingsPage = () => {
           <button>Delete</button>
         </div>
 
-        <div className="settingsInputsWrapper"></div>
+        <div className="settingsInputsWrapper">
+          <label>Full Name</label>
+          <input type="text" placeholder='Full Name' className='settingInputs'/>
+          <label>Gender</label>
+          <input type="text" placeholder='Gender' className='settingInputs'/>
+          <label>Location</label>
+          <input type="text" placeholder='Location' className='settingInputs'/>
+          <label>Phone Number</label>
+          <input type="text" placeholder='+234*********' className='settingInputs'/>
+          <label>Email</label>
+          <input type="text" placeholder='Example@gmail.com' className='settingInputs'/>
+          <label>Blood Type</label>
+          <input type="text" placeholder='A+' className='settingInputs'/>
+          <button className="editProfileButton">Edit Profile</button>
+        </div>
       </div>
 
-      <div className="SettingsPasswordsideWrapper"></div>
+      <div className="SettingsPasswordsideWrapper">
+      <div className="settingsInputsWrapper">
+          <label>Current Password</label>
+          <input type="text" placeholder='Enter Password' className='settingInputs'/>
+          <label>New Password</label>
+          <input type="text" placeholder='Enter Password' className='settingInputs'/>
+          <label>Confrm Password</label>
+          <input type="text" placeholder='Enter Password' className='settingInputs'/>
+          <button className="editProfileButton">Change Password</button>
+        </div>
+      </div>
     </div>
   )
 }
