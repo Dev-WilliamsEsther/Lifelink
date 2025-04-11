5c3ed3813f52f7dcbf68ac6781f77428ac397aaf
urigwesomto201 <coryguerrero500@gmail.com>
2025-04-11 12:14:50 -0500

    hospital details

--- src/Adio/pages/HospitalDetailsPage.jsx
+++ src/Adio/pages/HospitalDetailsPage.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './hospitalDetailsPage.css'
+import { Modal } from 'antd'
 
 const HospitalDetailsPage = () => {
+  const [volunteerPopUp, setVolunteerPopUp] = useState(false)
+
   return (
     <div className='HospitalDetailsPageWrapper'>
       <h1>Hospital Details</h1>
@@ -14,13 +17,27 @@ const HospitalDetailsPage = () => {
             <div className="innerTextWRapper">
                 <p>Name : <b>Kings Hospital</b></p>
                 <p>Location : <b>Oja Orile bustop, Lagos</b></p>
-                <p>Blood goup needed : <b>A+</b></p>
+                <p>Blood group needed : <b>A+</b></p>
                 <p>Contact : <b>09013717091</b></p>
                 <p>OPerating Hours : <b>"Mon-Fri, 8AM - 5PM"</b></p>
             </div>
-            <button>Volunteer to Donate</button>
+            <button onClick={()=> setVolunteerPopUp(true)}>Volunteer to Donate</button>
         </div>
       </div>
+      <Modal
+      open={volunteerPopUp}
+      onCancel={()=> setVolunteerPopUp(false)}
+      footer={null}
+      >
+        <div className="volunteerDateWrapper">
+          <label>Date</label>
+          <input type="date" />
+          <label>Time</label>
+          <input type="time" />
+          <button>Schedule</button>
+        </div>
+
+      </Modal>
     </div>
   )
 }
