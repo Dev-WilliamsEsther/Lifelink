1355f3f0a87ca4da09a0cf117e208b16d25cdc07
Your Name <you@example.com>
2025-09-23 18:28:13 +0100

    made update on the app

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -7,8 +7,8 @@ const Sponsors = () => {
       <div className='sponsorwrapper'>
         <h1>SPONSORS</h1>
         <div className="sponsorsLogos">
-          <img src="public/images/Sponsor8.png" alt="sponsor8" />
-          <img src="public/images/sponsor9.png" alt="sponsor9" />
+          <img src="/images/Sponsor8.png" alt="sponsor8" />
+          <img src="/images/Sponsor9.png" alt="sponsor9" />
         </div>
       </div>
     </>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import '../../components/footer/footer.css'
 import { PiUserCircleDashedThin } from 'react-icons/pi'
 import { IoMdMail } from "react-icons/io";
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { Link } from 'react-router'
 
 const Footer = () => {
   return (
     <div className='footerwrapper'>
       <div className='footerlogo'>
-        <div className='footerlogo1'>  <img src="images/logo.png" alt="LifeLink logo"  className='logo-breath'/> </div>
+        <div className='footerlogo1'>  <img src="images/logo.png" alt="LifeLink logo" className="logo-breath" /> </div>
 
         <h3>ALIFE</h3>
       </div>
@@ -16,10 +17,17 @@ const Footer = () => {
         <h1>Join the mission to save lives through blood donation.</h1>
         <div className='footerinput'>
 
-          <IoMdMail />
-          <p>alife.connect20@gmail.com</p>
+          <IoMdMail color='white' />
+          <p style={{ color: "white" }}>alife.connect20@gmail.com</p>
         </div>
-
+        <div className="footer-col">
+          <div className="social-icons">
+            <a href="#"><FaFacebookF /></a>
+            <a href="#"><FaTwitter /></a>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaLinkedinIn /></a>
+          </div>
+        </div>
       </div>
       <div className='footerfeatures'>
         <ul className='featureslist'>
@@ -33,10 +41,12 @@ const Footer = () => {
             <Link to="/howitworks">How it works</Link>
           </li>
           <li>Contact us</li>
-          <li>Privacy Policy</li>
-          <li>Sponsors</li>
         </ul>
       </div>
+      <div className="footer-bottom">
+        <p style={{ color: "white" }}>© 2025 ALIFE | All rights reserved.</p>
+      </div>
+
     </div>
   )
 }
